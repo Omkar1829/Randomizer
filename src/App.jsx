24717637818f2968f6b723fec1a388f4f917ec66
Omkar1829 <161331920+Omkar1829@users.noreply.github.com>
2025-07-10 19:19:19 +0530

    feat: Refactor App component to use React Router for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import Gamepage from './pages/Gamepage'
-import Gamepage2 from './pages/Gamepage2'
-import Offlinejackpot from './pages/Offlinejackpot'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Gamepage from './pages/Gamepage';
+import Gamepage2 from './pages/Gamepage2';
+import Offlinejackpot from './pages/Offlinejackpot';
 
 function App() {
-
   return (
-    <>
-    <Gamepage/>
-    {/* <Gamepage2/> */}
-    {/* <Offlinejackpot/> */}
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Gamepage />} />
+        <Route path="/offline" element={<Offlinejackpot />} />
+        {/* Optional route if you want to use Gamepage2 later */}
+        {/* <Route path="/gamepage2" element={<Gamepage2 />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
